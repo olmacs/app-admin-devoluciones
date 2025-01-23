3e2951c42ba0c74936e0e463d5e4d5a20cd7facb
John Martinez <johnmartinez.jm.jm@gmail.com>
2025-01-23 09:34:09 -0500

    feat: integrate GraphQL query to fetch returns data in UsersTable component

--- react/UsersTable/index.tsx
+++ react/UsersTable/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { EXPERIMENTAL_Table as Table } from 'vtex.styleguide'
-import faker from 'faker'
+import { EXPERIMENTAL_Table as Table, Spinner } from 'vtex.styleguide'
+// import faker from 'faker'
+import { useQuery } from 'react-apollo'
+
+import GET_RETURNS from '../graphql/GetReturn.gql'
 /** Columns definition, must be an array */
 const columns = [
   {
@@ -12,41 +15,36 @@ const columns = [
     width: '3rem',
   },
   {
-    id: 'name',
-    title: 'Name',
+    id: 'state',
+    title: 'Estado',
   },
   {
-    id: 'qty',
-    title: 'Qty',
-  },
-  {
-    id: 'costPrice',
-    title: 'Cost',
-  },
-  {
-    id: 'retailPrice',
-    title: 'Retail',
+    id: 'usuario_correo',
+    title: 'User',
   },
 ]
 
-const EXAMPLE_LENGTH = 10
-const MOCKED_DATA = [...Array(EXAMPLE_LENGTH)].map(() => ({
-  id: faker.random.uuid(),
-  name: faker.name.findName(),
-  qty: faker.random.number(),
-  costPrice: faker.random.number(),
-  retailPrice: faker.random.number(),
-}))
+// const EXAMPLE_LENGTH = 10
+// const MOCKED_DATA = [...Array(EXAMPLE_LENGTH)].map(() => ({
+//   id: faker.random.uuid(),
+//   name: faker.name.findName(),
+//   qty: faker.random.number(),
+//   costPrice: faker.random.number(),
+//   retailPrice: faker.random.number(),
+// }))
 
 const rowClick = ({ rowData }: { rowData: any }) => {
   console.info(`Clicked ${rowData.id}`)
   alert(`Clicked ${rowData.name} from ${rowData.location}`)
 }
 export default function UsersTable() {
+  const { data, loading, error } = useQuery(GET_RETURNS)
+  if (loading) return <Spinner />
+  if (error) return <div>{error.message}</div>
   return (
     <Table
       measures="regular"
-      items={MOCKED_DATA}
+      items={data.getAllReturns}
       columns={columns}
       onRowClick={rowClick}
     />
